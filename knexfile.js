38c34f43f4d47b524096e52bc22e097963538791
Justin <jdlow2@gmail.com>
2019-01-18 11:21:51 -0500

    migrations, server up and running

--- knexfile.js
+++ knexfile.js
@@ -5,14 +5,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/db.sqlite3'
-    }
+      filename: './data/db.sqlite3',
+    },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations/'
+      directory: './data/migrations/',
     },
     seeds: {
       directory: './data/seeds',
-    }
+    },
   },
 };
