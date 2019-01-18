9c12aa8c2ee0d51c8bf76d24234b024fb784211b
Justin <jdlow2@gmail.com>
2019-01-18 11:59:36 -0500

    GET endpoint for projects.
    But need to fix formatting output.

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -18,5 +18,21 @@ router.post('/', (req, res) => {
   }
 });
 
+router.get('/:id', (req, res) => {
+  db('projects as p')
+    .join('actions as a', 'a.project_id', 'p.id')
+    .where({ 'p.id': req.params.id })
+    .then((project) => {
+      if (!project.length) {
+        res.status(400).json({ error: 'invalid project id' });
+      } else {
+        res.status(200).json(project);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: `there was an error retrieving the project: ${err}` });
+    });
+});
+
 
 module.exports = router;
