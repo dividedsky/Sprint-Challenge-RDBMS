4c6e9b1267d56207d314d1946ffbcd49cc3e69ef
Justin <jdlow2@gmail.com>
2019-01-18 12:11:04 -0500

    Fixed output at project GET endpoint

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -19,18 +19,38 @@ router.post('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  db('projects as p')
-    .join('actions as a', 'a.project_id', 'p.id')
-    .where({ 'p.id': req.params.id })
-    .then((project) => {
-      if (!project.length) {
-        res.status(400).json({ error: 'invalid project id' });
-      } else {
-        res.status(200).json(project);
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ error: `there was an error retrieving the project: ${err}` });
+  /*
+   *db('projects as p')
+   *  .join('actions as a', 'a.project_id', 'p.id')
+   *  .where({ 'p.id': req.params.id })
+   *  .then((project) => {
+   *    if (!project.length) {
+   *      res.status(400).json({ error: 'invalid project id' });
+   *    } else {
+   *      res.status(200).json(project);
+   *    }
+   *  })
+   *  .catch((err) => {
+   *    res.status(500).json({ error: `there was an error retrieving the project: ${err}` });
+   *  });
+   */
+  const project = db('projects')
+    .where({ id: req.params.id }).first();
+
+  const actions = db('actions')
+    .where({ project_id: req.params.id });
+
+  Promise.all([project, actions])
+    .then((output) => {
+      console.log(output);
+      const [project, actionsArr] = output;
+      res.status(200).json({
+        id: project.id,
+        name: project.name,
+        description: project.description,
+        completed: project.completed,
+        actions: actionsArr,
+      });
     });
 });
 
